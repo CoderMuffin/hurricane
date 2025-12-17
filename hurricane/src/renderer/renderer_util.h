0242bc82987cd3e03cdb9fb9e79808d7fe89d20d
CoderMuffin <codermuffin@gmail.com>
2025-12-17 11:24:41 +0000

    change abs to fabs

--- hurricane/src/renderer/renderer_util.h
+++ hurricane/src/renderer/renderer_util.h
@@ -62,7 +62,7 @@ double calculate_depth(double x0, double y0, double z0, double x1, double y1,
                        double py) {
   double w_denom = (y1 - y2)*(x0 - x2) + (x2 - x1)*(y0 - y2);
 
-  if (abs(w_denom) < 1.0) {
+  if (fabs(w_denom) < 1.0) {
     return INFINITY;
   }
 
